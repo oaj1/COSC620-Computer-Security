ad77a9b28d2610ce190c289c5e154c32610359e2
Oliver James <31637093+oaj1@users.noreply.github.com>
2021-10-10 15:03:57 -0400

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,26 +1,26 @@
 #Take input file, make into list of bigrams, sort to top 20 in descending order
 
-from operator import itemgetter
+ffrom operator import itemgetter
 
-filePtr = open('body.txt', 'r')
-text = "" #collect text one line at a time
-while True:
-        # Get next line from file
-        line = filePtr.readline()
-        # if line is empty
-        # end of file is reached
-        if not line:
-            break
-        text = text.strip() + " " + line.lower().strip()
-filePtr.close()
+file = open("body.txt") #this will be the file holding the articles
+
+
+
+text = file.read()#read the file
+text = text.lower()#make each word lower case
+text = text.replace("\r\n"," ").replace("\n"," ").replace("  "," ")
 
 content = text #content is variable used for creating the list
 contentList = content.split(" ")#split each new word on a blank space
 
-bigrams = [] #bigrams will be used to place list of bigrams
+
+
+bigrams = []
 for i in range(len(contentList)-1):
     temp = (contentList[i],contentList[i+1]) #Manually append to bigrams list the various bigrams
     bigrams.append(temp)
+
+
 
 frequency = {}#dictionary, and is utlized to count the number of times bigram occurs
 for item in bigrams:
@@ -34,8 +34,15 @@ for item in bigrams:
     else:
       # initializing the count
 
-        frequency[item] = 1
+        frequency[item] = 1 
+
+
+
+
 frequency = sorted(frequency.items(),key = lambda x:x[1],reverse = True)
+
+
+print()
 print("Top 20 descending order bigram")
 res = dict(sorted(frequency, key = itemgetter(1), reverse = True)[:20])
 print(res)
